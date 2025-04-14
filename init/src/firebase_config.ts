25a6b3d40588714d8a98e5bd01b053f85d90737b
voloder <110066198+voloder@users.noreply.github.com>
2025-04-14 22:47:17 +0200

    add firecms

---
+++ init/src/firebase_config.ts
@@ -0,0 +1,11 @@
+export const firebaseConfig = {
+    "projectId": "konjic-discover",
+    "appId": "1:342680650401:web:bf725e9d303477da23e6e0",
+    "databaseURL": "https://konjic-discover-default-rtdb.europe-west1.firebasedatabase.app",
+    "storageBucket": "konjic-discover.appspot.com",
+    "locationId": "europe-west3",
+    "apiKey": "AIzaSyBhzONrkAvgHjPTduWffZlHZXPfXTJBjFI",
+    "authDomain": "konjic-discover.firebaseapp.com",
+    "messagingSenderId": "342680650401",
+    "measurementId": "G-HB73KQEHMQ"
+}
